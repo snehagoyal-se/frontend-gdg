1b4a5e497fc3be1a865557b44350ea14e0488050
Sneha Goyal <snehagoyal9084@gmail.com>
2026-08-21 18:54:42 +0530

    modified vercel error

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Navbar from "./components/Navbar"
+import Navbar from "./components/navbar"
 import Hero from "./components/hero_section"
 import HowVertextWorks from "./components/vertex_works"
 import FeatureSection from "./components/feature"
